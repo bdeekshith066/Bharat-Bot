948c6c00e048cf239a7f38f56922575d168c8704
Deekshith B <130782878+bdeekshith066@users.noreply.github.com>
2024-02-25 00:17:17 +0530

    Update streamlit_chat_app.py

--- streamlit_chat_app.py
+++ streamlit_chat_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import openai
 from googletrans import Translator
 
-# Set your OpenAI API key
-openai.api_key = 'enter your openai apii key' # Insert your OpenAI API key here
 
 # Typing animation HTML code with "Project by Deekshith B"
 typing_animation = """
@@ -14,44 +12,6 @@ typing_animation = """
 
 # Display the typing animation
 st.markdown(typing_animation, unsafe_allow_html=True)
-
-# Display the images
-
-st.markdown(
-    """
-    <style>
-    .center {
-        display: flex;
-        justify-content: center;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.markdown(
-    """
-    <div class="center">
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png" width="150">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
-
-# Main function
-
-# Function to translate text to English
-def translate_to_english(text):
-    translator = Translator()
-    translated_text = translator.translate(text, dest='en')
-    return translated_text.text
-
-# Function to translate text to the desired language
-def translate_to_lang(text, dest_lang):
-    translator = Translator()
-    translated_text = translator.translate(text, dest=dest_lang)
-    return translated_text.text
 
 # Function to generate response using GPT-3
 def generate_response(prompt, input_lang, output_lang):
@@ -95,26 +55,42 @@ supported_languages = {
     'awa': 'Awadhi', 
 }
 
-# Main function
+# Function to translate text to English
+def translate_to_english(text):
+    translator = Translator()
+    translated_text = translator.translate(text, dest='en')
+    return translated_text.text
 
+# Function to translate text to the desired language
+def translate_to_lang(text, dest_lang):
+    translator = Translator()
+    translated_text = translator.translate(text, dest=dest_lang)
+    return translated_text.text
 
 def main():
-    st.title("Bharat Bot 🇮🇳🤖")  
+    # Set your OpenAI API key
+   
+    openai.api_key = st.sidebar.text_input("Enter your OpenAI API key:", type="password")
+
+    st.title("Bharat Bot 🇮🇳🤖")
     col1, col2 = st.columns([2, 1])
     col1.write("Bharat Bot connects people across India by comprehending")
-    col2.image("https://www.ccjk.com/wp-content/uploads/2021/03/How-many-languages-are-spoken-in-India.png",caption="Unity in Diversity", width=200)
+    col2.image("https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png", width=150)
     col1.write("and embracing the diverse spectrum of Indian languages.")
+    
+    st.sidebar.image("https://www.ccjk.com/wp-content/uploads/2021/03/How-many-languages-are-spoken-in-India.png",caption="Unity in Diversity", width=300)
+    input_lang = st.sidebar.selectbox("Choose your input language:", list(supported_languages.keys()), format_func=lambda x: f"{x} - {supported_languages[x]}")
+    output_lang = st.sidebar.selectbox("Choose your output language:", list(supported_languages.keys()), format_func=lambda x: f"{x} - {supported_languages[x]}")
 
-    input_lang = st.selectbox("Choose your input language:", list(supported_languages.keys()), format_func=lambda x: f"{x} - {supported_languages[x]}")
-    output_lang = st.selectbox("Choose your output language:", list(supported_languages.keys()), format_func=lambda x: f"{x} - {supported_languages[x]}")
-
-    for i in range(5):
+    for i in range(10):
         user_input = st.text_input(f"Enter your question {i+1}:", key=f"question_{i+1}")
 
         if st.button(f"Submit {i+1}"):
-            chatgpt_response = generate_response(user_input, input_lang, output_lang)
-            st.write(f"Response {i+1}:", chatgpt_response)
-
+            if openai.api_key:
+                chatgpt_response = generate_response(user_input, input_lang, output_lang)
+                st.write(f"Response {i+1}:", chatgpt_response)
+            else:
+                st.error("Please enter your OpenAI API key.")
 
 if __name__ == "__main__":
     main()
